cc426f7ddea777b9beafb2fbbc319365ad39a57b
rafaelflorespereira <rafael_530@hotmail.com>
2020-12-10 19:43:49 -0300

    returning music genre by temperature

--- spotify-api/app/Http/Controllers/spotifyController.php
+++ spotify-api/app/Http/Controllers/spotifyController.php
@@ -12,18 +12,20 @@ class spotifyController extends Controller
      * @param Request
      */
     public function main(Request $request) {
+        //validate data return the error if the response wasnt 200.
+        $temperature = 0;
         if($request->cidade != null) {
             $temperature = self::getTemperatureByCityName($request);
-            dd($temperature);
         }
         elseif ($request->lat != null && $request->lon != null) {
             $temperature = self::getTemperatureByLatLon($request);
-            dd($temperature);
         }
+
+        $musicGenre = self::getMusicGenre($temperature);
+        dd($musicGenre);
     }
-    
+
     public function getTemperatureByCityName(Request $request) {
-        //validate data return the error if the response wasnt 200.
         //hide api KEY
         $response = Http::get('api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units={unidade}', [
             'q' => $request->cidade,
@@ -55,6 +57,23 @@ class spotifyController extends Controller
      * @return string genre
      */
     public function getMusicGenre($temperature) {
+        if ($temperature > 30) {
+            return 'festa';
+        } elseif ($temperature >= 15) {
+            return 'pop';
+        } elseif ($temperature > 10) {
+            return 'rock';
+        } else {
+            return 'classicas';
+        }
+    }
 
+    // Client ID 71e5072f3ef34b25bf965f1eda24e441
+    // Client Secret 03dd54a0503e423384974abeab874592
+    public function getPlaylist() {
+        $musicGenre = 'mood';
+        $token = 'BQBa5FcKGZIVhf30ns-mwfsrfokCnkqRZ5e8AtRf9c0JFATj4GkQXNbsEc9xvLXF697msC_2ZNshNx8kY3Zmta5EDjuWbv2EDebpUFsoIiVkcwBKIXYATq73FCbqojUEDOOCCVBnK00oYdrnR7--4LidsNjujDYpihjAX_6Efc4_BJAb5ylATgouvArfEdPCjRDM0HvlRN-ZNHuCRZgT7x9esKorw2Uwkc54fisLQE-K_Azhkso1NxssU0pPu5yk2DgrsDjEW-iB6C1G';
+        $response = Http::get('https://api.spotify.com/v1/browse/categories/'. $musicGenre .'/playlists', 
+        )->header('Bearer-Token', $token)->json();
     }
 }
